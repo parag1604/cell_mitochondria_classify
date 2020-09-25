d5eb5f5622cb241242105337a2bff4aa7c74a814
Parag Dutta <parag1604@hotmail.com>
2020-09-25 22:09:55 +0530

    driver file added -> run.py

---
+++ run.py
@@ -0,0 +1,27 @@
+import sys, os
+
+######################
+# MODE 1 - PREPROCESS
+# MODE 2 - CLASSIFY
+######################
+
+def check_and_get_system_args():
+    if len(sys.argv) == 2:
+        return int(sys.argv[1])
+    print("Usage: python run.py <mode=1|2>")
+    sys.exit(1)
+
+def main(mode):
+    if mode == 1:
+        command = "python preprocess.py setting.json"
+    elif mode == 2:
+        command = "python classify.py settings.json"
+    else:
+        print("INVALID MODE OPTION PROVIDED")
+        print("VALID:\nMODE 1 - PREPROCESS\nMODE 2 - CLASSIFY")
+        sys.exit(2)
+    os.system()
+
+if __name__ == "__main__":
+    main(check_and_get_system_args())
+    sys.exit(0)
